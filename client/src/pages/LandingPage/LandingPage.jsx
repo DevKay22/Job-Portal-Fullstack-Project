5320d8f1c01a9ee956d8edb441180778c75f6be8
DevKay22 <drkay4sure@gmail.com>
2025-09-19 15:37:13 +0100

    added features section

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Header from './Components/Header'
+import Header from './components/Header'
 import Hero from './components/Hero'
+import Feature from './components/Feature'
+// import Header from './components/Header'
 
 const LandingPage = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen mb-[100vh]'>
+        
         <Header />
         <Hero />
+        <Feature />
     </div>
   )
 }
