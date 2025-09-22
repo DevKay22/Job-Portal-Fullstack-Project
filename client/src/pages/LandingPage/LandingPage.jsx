c134f0fda5b857b0c150fa61664582637d07028b
DevKay22 <drkay4sure@gmail.com>
2025-09-22 10:15:45 +0100

    Analytics feature

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Feature from './components/Feature'
+import Analytics from './components/Analytics'
 // import Header from './components/Header'
 
 const LandingPage = () => {
@@ -11,6 +12,7 @@ const LandingPage = () => {
         <Header />
         <Hero />
         <Feature />
+        <Analytics />
     </div>
   )
 }
